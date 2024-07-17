064e487180eb7eecd212f4c1417934be10fafbde
Samgrass <samuelegrassetti01@gmail.com>
2024-07-17 11:20:28 +0200

    aggiunto mockup AppFooter

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -6,7 +6,77 @@ export default {
 </script>
 
 <template>
+    <footer>
+        <!--  parte clients con carosello -->
+        <section>
+            <div class="container text-center">
+                <div class="row">
+                    <div class="col">
+                        <h3>Why Anidio studio?</h3>
+                        <h2>We Love Our Clients </h2>
+                        <p>We are trusted throughout adland and have a wealth of recommendations from start-ups to renowned global brands. </p>
+                        <div>
+                            <!-- parte di carosello -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <div class="container text-center">
+            <div class="row">
+                <div class="col">
+                    <!-- parte enter email -->
+                    <h3>We Create New Worlds! </h3>
+                    <h2>Want Tips On Your Storytelling? </h2>
+                    <p>Join our newsletter for tips on how to elevate your brand through storytelling</p>
+                    <form action="email">
+                        <input type="email" placeholder="Enter your email">
+                        <input type="submit" value="email">
+                    </form>
+                </div>
+            </div>
+        </div>
+        <section>
+
+        </section>
+        <!-- parte navfooter e socials -->
+        <section class="ms_footer-socials">
+            <div class="container">
+                <div class="row justify-content-between">
+                    <div class="col">
+                        ciao
+                    </div>
+                    <div class="col">
+                        <ul class="d-flex justify-content-end">
+                            <li>ciao</li>
+                            <li>ciao</li>
+                            <li>ciao</li>
+                            <li>ciao</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="interline">linea</div>
+                <div class="row justify-content-between">
+                    <div class="col">
+                        ciao
+                    </div>
+                    <div class="col">
+                        <ul class="d-flex justify-content-end">
+                            <li>ciao</li>
+                            <li>ciao</li>
+                            <li>ciao</li>
+                            <li>ciao</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </footer>
+    
+
 
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+</style>
