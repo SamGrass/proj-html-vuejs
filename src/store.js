26f26626043037a54e4dc8e2ac93a509de4e2edb
Samgrass <samuelegrassetti01@gmail.com>
2024-07-17 12:56:54 +0200

    creato store.js, aggiunta lista immagini sponsor

---
+++ src/store.js
@@ -0,0 +1,24 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+    sponsorList: [
+        {
+            imageUrl: '/public/sponsor1.png',
+        },
+        {
+            imageUrl: '/public/sponsor2.png',
+        },
+        {
+            imageUrl: '/public/sponsor3.png',
+        },
+        {
+            imageUrl: '/public/sponsor4.png',
+        },
+        {
+            imageUrl: '/public/sponsor5.png',
+        },
+        {
+            imageUrl: '/public/sponsor6.png',
+        }
+    ]
+})
