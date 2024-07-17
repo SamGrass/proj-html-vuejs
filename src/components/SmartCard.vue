c58333f7201cf3f90d29db63703c7794c66b64f7
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-07-17 13:04:39 +0200

    feat(SmartCard.vue): componente card della sezione smart completata

---
+++ src/components/SmartCard.vue
@@ -0,0 +1,63 @@
+<script>
+export default {
+    name: 'SmartCard',
+
+    props: {
+        imagePath: String,
+        titleURL: String,
+        titleText: String,
+        paragraphText: String
+    }
+}
+</script>
+
+<template>
+    <div class="smart-card">
+        <div class="thumbnail">
+            <img :src="imagePath" :alt="titleText">
+        </div>
+
+        <h4>
+            <a :href="titleURL">
+                {{ titleText }}
+            </a>
+        </h4>
+
+        <p>
+            {{ paragraphText }}
+        </p>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+
+.smart-card {
+    width: 100%;
+    height: 100%;
+    border-radius: $rad-xl;
+    padding: 2.25rem 1rem;
+    border: 1px solid #b2b5d2;
+    background: linear-gradient(180deg, #ffffff, #fff1e8), #fff1e8;
+    box-shadow: 0 .5rem 1.125 0 #f6f3ff9c;
+
+    &:hover {
+        border-color: #ffffff;
+        background: linear-gradient(0deg, #ffffff, #fff1e8), #fff1e8;
+
+    }
+
+    .thumbnail {
+        height: 5rem;
+        margin-bottom: 2rem;
+
+        img {
+            height: 100%;
+        }
+    }
+
+    h4 {
+        margin-bottom: .625rem;
+    }
+}
+</style>
