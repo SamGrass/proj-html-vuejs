30f5d15a15a59238495f24443e7bacc535ab4397
Samgrass <samuelegrassetti01@gmail.com>
2024-07-17 17:22:47 +0200

    aggiunto stile a newsletter

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -27,12 +27,13 @@ export default {
                 </div>
             </div>
         </section>
-        <!-- aggiungere onde -->
-        <!-- parte enter email -->
+        
+        <!-- parte newsletter -->
         <section class="ms_newsletter">
+            <!-- aggiungere onde con css-->
             <div class="container text-center">
                 <div class="row">
-                    <div class="col">                       
+                    <div class="col-5 offset-4 ms_text-newsletter">                       
                         <h3>We Create New Worlds! </h3>
                         <h2>Want Tips On Your Storytelling? </h2>
                         <p>Join our newsletter for tips on how to elevate your brand through storytelling</p>
@@ -43,14 +44,15 @@ export default {
                     </div>
                 </div>
             </div>
+            <!-- aggiungere onde con css -->
         </section>
-        <!-- aggiungere onde -->
+        
         <!-- parte navfooter e socials -->
         <section class="ms_footer-socials">
             <div class="container">
                 <div class="row justify-content-between">
                     <div class="col">
-                        ciao
+                        <img src="/footer-logo.png" alt="">
                     </div>
                     <div class="col">
                         <ul class="d-flex justify-content-end">
@@ -86,14 +88,51 @@ export default {
 <style lang="scss" scoped>
 @use '../styles/partials/variables' as *;
 
-    section {
-        padding: 2rem 0;
-    }
+    
 
     .ms_newsletter {
-        // debug
-        background-color: blue;
+        position: relative;
+        padding: 5rem 0;
+        background-image: url(counter-bg.png);
+        background-repeat: no-repeat;
+        background-size: cover;
+        color: #ffffff;
+        overflow: hidden;
+
+        .container {
+            position: relative;
+            z-index: 3;
+        }
+
+        &::before,
+        &::after {
+            content: '';
+            background-repeat: no-repeat;
+            background-size: 100% auto;
+            position: absolute;
+            top: 0;
+            right: 0;
+            width: 100%;
+            height: 100%;
+            z-index: 2;
+        }
+
+        &::before {
+            background-image: url(/shape-top.png);
+            background-position: top;
+        }
+
+        &::after {
+            background-image: url(/shape-bottom.png);
+            background-position: bottom;
+        }
+
+        h3 {
+            color: $orange;
+        }
     }
+
+    
     .ms_footer-socials {
         padding: 2rem 0 1rem;
 
