111e516cd25e592e0619867c867249c3f70ab857
Samgrass <samuelegrassetti01@gmail.com>
2024-07-18 16:49:19 +0200

    creata lista per immagini in Service

--- src/store.js
+++ src/store.js
@@ -42,5 +42,37 @@ export const store = reactive({
             name: 'Hector Vargas',
             role: 'Project Lead & Senior Animator'
         },
+    ],
+    serviceList: [
+        {
+            image: '/process1.png',
+            title: 'Motion Graphics',
+            paragraph: 'The best in the business, served to your audience’s eyeballs.'
+        },
+        {
+            image: '/process2.png',
+            title: 'Character Design',
+            paragraph: 'The best in the business, served to your audience’s eyeballs. '
+        },
+        {
+            image: '/smart1.png',
+            title: 'Storyboards',
+            paragraph: 'The best in the business, served to your audience’s eyeballs. '
+        },
+        {
+            image: '/smart2.png',
+            title: 'Animatics',
+            paragraph: 'The best in the business, served to your audience’s eyeballs.'
+        },
+        {
+            image: '/smart3.png',
+            title: 'Film and TV',
+            paragraph: 'The best in the business, served to your audience’s eyeballs.'
+        },
+        {
+            image: '/smart1.png',
+            title: 'Script writing',
+            paragraph: 'The best in the business, served to your audience’s eyeballs.'
+        },
     ]
 })
