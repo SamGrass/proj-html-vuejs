96cecea39ae08ec2532115a92c1db3b0f153ddf5
alessandrofogliada <fogliada.a@gmail.com>
2024-07-18 18:28:31 +0200

    Inizio JumbotronContacts

---
+++ src/components/JumbotronContacts.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+    name:'JumbotronContacts',
+}
+</script>
+
+
+<template>
+                <div class="row">
+                    <div class="offset-2 col-xl-9 d-flex justify-content-between title">
+
+                        <div>
+                            <h1>Contacs</h1>
+                            <span>Home <i class="fa-solid fa-arrow-right-long"></i> Contacs </span>
+                       </div>
+
+                       <!-- CERCA IMG DI RAGAZZA CHE MANCA  -->
+                         <img src="/public/dots-right.png" alt="">
+                    </div>
+                </div>
+</template>
+
+
+<style scoped lang="scss">
+.title{
+    width: 80%;
+    margin: 0 auto;
+    margin-top: 5rem;
+}
+
+img{
+    width: 40%;
+}
+
+h1{ 
+    margin-bottom: 1rem;
+}
+
+</style>
