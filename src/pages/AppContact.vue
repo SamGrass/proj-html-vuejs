01e84c1f4ec7f52966fbc0c217f4327a3486e4b7
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-07-19 17:15:05 +0200

    feat(AppContact.vue): pagina Contact Us popolata dei rispettivi componenti

--- src/pages/AppContact.vue
+++ src/pages/AppContact.vue
@@ -1,12 +1,20 @@
 <script>
+import ContactSection from '../components/ContactSection.vue';
+import FAQSection from '../components/FAQSection.vue';
+
 export default {
     name: 'AppContact',
 
+    components: {
+        ContactSection,
+        FAQSection
+    }
 }
 </script>
 
 <template>
-
+    <ContactSection />
+    <FAQSection />
 </template>
 
 <style lang="scss" scoped></style>
