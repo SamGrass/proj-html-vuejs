0394ec869cc8c8988c28b79fac7cf59b9f44fc62
Samgrass <samuelegrassetti01@gmail.com>
2024-07-19 12:36:31 +0200

    cambiato nome in sponsor card e refactoring contenuto per renderlo in linea con le altre card

---
+++ src/components/SponsorCard.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    name: 'SponsorCard',
+    props: {
+        image: String,
+    }
+}
+</script>
+
+<template>
+    
+    <div class="ms_card">
+        <img :src="image" :alt="image">
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+
+.ms_card {
+    min-width: 200px;
+}
+       
+    
+</style>
